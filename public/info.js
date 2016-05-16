4f1d69dc9ff38b545d8a99f67e7b4e89edd081d2
manu_r <manuel.ritz@hs-augsburg.de>
2016-05-16 19:01:00 +0200

    add index.html for information about the REST API

---
+++ public/info.js
@@ -0,0 +1,8 @@
+﻿//angular.js infoController
+function info($scope, $http) {
+    var app = require('../server').app;
+
+    $http.get(app.url).success(function(data) {
+            $scope.infos = data;
+        });
+}
